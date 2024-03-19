8e84c74be5f77d756940fd6bfe36e3738dd98f03
dbug-entity <vishwa.codes@gmail.com>
2024-03-20 03:23:51 +0500

    add image to personal information page

--- src/components/PersonalInformation.vue
+++ src/components/PersonalInformation.vue
@@ -1,50 +1,60 @@
 // PersonalInformation.vue
 <template>
   <div class="bg-blue-900 min-h-screen flex items-center justify-center">
-    <div class="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-black">
-      <h2 class="text-2xl font-semibold mb-6 text-center">
-        Add Personal Information
-      </h2>
-      <form @submit.prevent="nextStep">
-        <div class="mb-4">
-          <label for="firstName" class="block font-medium mb-1">Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            v-model="formData.name"
-            required
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="email" class="block font-medium mb-1">Email:</label>
-          <input
-            type="email"
-            id="email"
-            v-model="formData.email"
-            required
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="phoneNumber" class="block font-medium mb-1"
-            >Password:</label
+    <div
+      class="bg-white p-8 rounded-lg shadow-lg max-w-3xl mx-auto flex flex-col md:flex-row md:items-center"
+    >
+      <img
+        :src="imagePath"
+        alt="Image"
+        class="object-cover w-full h-auto rounded-lg md:w-1/2 md:h-full"
+      />
+
+      <div class="w-full px-4 md:w-1/2 md:px-8">
+        <h2 class="text-2xl font-semibold mb-6 text-center">
+          Add Personal Information
+        </h2>
+        <form @submit.prevent="nextStep">
+          <div class="mb-4">
+            <label for="firstName" class="block font-medium mb-1">Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              v-model="formData.name"
+              required
+              class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block font-medium mb-1">Email:</label>
+            <input
+              type="email"
+              id="email"
+              v-model="formData.email"
+              required
+              class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="phoneNumber" class="block font-medium mb-1"
+              >Password:</label
+            >
+            <input
+              type="tel"
+              id="phoneNumber"
+              v-model="formData.password"
+              required
+              class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
+            />
+          </div>
+          <button
+            type="submit"
+            class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
-          <input
-            type="tel"
-            id="phoneNumber"
-            v-model="formData.password"
-            required
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 bg-white"
-          />
-        </div>
-        <button
-          type="submit"
-          class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-        >
-          Next
-        </button>
-      </form>
+            Next
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -65,45 +75,12 @@ export default defineComponent({
       router.push("/subscription");
     };
 
-    return { formData, nextStep };
+    return { formData, nextStep, imagePath: require("@/assets/5500661.jpg") };
   },
 });
 </script>
 
 <style scoped>
-@import "tailwindcss/base";
-@import "tailwindcss/components";
-@import "tailwindcss/utilities";
-
-.button {
-  --😀: #644dff;
-  --😀😀: #4836bb;
-  --😀😀😀: #654dff63;
-  cursor: pointer;
-  width: 184px;
-  height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  font-size: 1.125em;
-  font-weight: 800;
-  letter-spacing: 2px;
-  color: #fff;
-  background: var(--😀);
-  border: 2px solid var(--😀😀);
-  border-radius: 0.75rem;
-  box-shadow: 0 8px 0 var(--😀😀);
-  transform: skew(-10deg);
-  transition: all 0.1s ease;
-  filter: drop-shadow(0 15px 20px var(--😀😀😀));
-}
-
-.button:active {
-  letter-spacing: 0px;
-  transform: skew(-10deg) translateY(8px);
-  box-shadow: 0 0 0 var(--😀😀😀);
-}
 .bg-blue-900 {
   background-color: #284276;
 }
